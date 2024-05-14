5fd1eeaacd3745d06b1680580504d4832f1dbd74
VietHa <havietnguyen2002@gmail.com>
2024-05-14 20:29:54 +0700

    fix error firebase

--- js/main.js
+++ js/main.js
@@ -14,20 +14,7 @@ const customParamValue = getQueryParam('custom_param');
 // document.body.appendChild(customParamElement);
 // Import the functions you need from the SDKs you need
 
-const firebaseConfig = { 
-  apiKey : "AIzaSyCj8R0iJmoT-hlfETLGdTYxzk5VUQ9CLBw" , 
-  authDomain : "mechatronic-project-af507.firebaseapp.com" , 
-  databaseURL : "https://mechatronic-project-af507-default-rtdb.firebaseio.com" , 
-  projectId : "mechatronic-project-af507" , 
-  storageBucket : "mechatronic-project-af507.appspot.com" , 
-  messagingSenderId : "782997268535" , 
-  appId : "1:782997268535:web:0f36553a1637a1400977b2" 
-};  
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const auth = firebase.auth();
 
 var valueDayExport = firebase.database().ref(customParamValue).child("key1");
 valueDayExport.on("value", (snap) => {
