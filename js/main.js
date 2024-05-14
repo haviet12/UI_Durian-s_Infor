9523a2f2d32a2662f5867bf87aae237fabd19383
VietHa <havietnguyen2002@gmail.com>
2024-05-15 00:08:23 +0700

    test

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 // Hàm để lấy giá trị của một tham số từ URL
-function getQueryParam(param) {
-  const queryString = window.location.search;
-  const urlParams = new URLSearchParams(queryString);
-  return urlParams.get(param);
-}
+// function getQueryParam(param) {
+//   const queryString = window.location.search;
+//   const urlParams = new URLSearchParams(queryString);
+//   return urlParams.get(param);
+// }
 
-// Lấy giá trị của tham số "custom_param" từ URL
-const customParamValue = getQueryParam('custom_param');
+// // Lấy giá trị của tham số "custom_param" từ URL
+// const customParamValue = getQueryParam('custom_param');
 
-// Hiển thị giá trị của tham số "custom_param" lên trang web
-const customParamElement = document.createElement('p');
-customParamElement.textContent = 'Giá trị của tham số custom_param là: ' + customParamValue;
-document.body.appendChild(customParamElement);
+// // Hiển thị giá trị của tham số "custom_param" lên trang web
+// const customParamElement = document.createElement('p');
+// customParamElement.textContent = 'Giá trị của tham số custom_param là: ' + customParamValue;
+// document.body.appendChild(customParamElement);
 
 
 
@@ -20,7 +20,7 @@ document.body.appendChild(customParamElement);
 // });
 
 
-var valueDayExport = firebase.database().ref(customParamValue).child("Name");
+var valueDayExport = firebase.database().ref("Sample1").child("Name");
 valueDayExport.on("value", (snap) => {
   console.log("Day: " + snap.val());
   document.getElementById("day").innerHTML = snap.val();
